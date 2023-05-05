3547ce00371cee59c2cc209a1d0b08c77438e526
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-05 21:10:28 +0900

    Add basic validation

--- frontend/views/AddCardView.vue
+++ frontend/views/AddCardView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <div class="error-message" v-show="errorMessage">{{ errorMessage }}</div>
+    <ul v-for="error in errors" :key="error">
+      <li>{{ error }}</li>
+    </ul>
     <form @submit="onSubmit" id="my-form">
       <div class="field">
         <label>English</label>
@@ -27,18 +30,38 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import type { Ref } from 'vue'
 
+const errors: Ref<string[]> = ref([])
 const english = ref('')
 const kana = ref('')
 const kanji = ref('')
 const errorMessage = ref('')
 let showSuccessMessage = ref(false)
 
+function isValid(): boolean {
+  errors.value = []
+
+  if (english.value == null || english.value.length === 0) {
+    errors.value.push('English answer must be supplied')
+  }
+
+  if (
+    (kana.value == null || kana.value.length == 0) &&
+    (kanji.value == null || kanji.value.length == 0)
+  ) {
+    errors.value.push('Kana or Kanji must be supplied')
+  }
+
+  return errors.value.length > 0 ? false : true
+}
+
 async function onSubmit(event: Event) {
   event.preventDefault()
 
-  //   TODO: validate the inputs
-  //    english should be mandatory, and either kana OR kanji should be mandatory
+  if (!isValid()) {
+    return
+  }
 
   const response = await window.electronAPI.createCard({
     english: english.value,

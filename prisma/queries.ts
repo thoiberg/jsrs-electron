036f1e8f707eab1a2587a463a955ee1352a909a4
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-06 00:11:44 +0900

    Improve guard function for error return probably

--- prisma/queries.ts
+++ prisma/queries.ts
@@ -36,13 +36,20 @@ export async function createCard(
     let error
     if (e instanceof Error) {
       error = e
-    } else if (Object.prototype.hasOwnProperty.call(e, 'message')) {
-      const boop = e as { message: string }
-      error = Error(boop.message)
+    } else if (hasMessage(e)) {
+      error = Error(e.message)
     } else {
       error = Error('something went wrong')
     }
 
     return { error }
   }
+}
+
+function hasMessage(x: unknown): x is { message: string } {
+  if (x && typeof x === 'object' && 'message' in x) {
+    return true
+  }
+
+  return false
 }

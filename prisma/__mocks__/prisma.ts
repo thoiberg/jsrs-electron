e4f7c94983370885564506f52466b7e96a0a7a42
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-06 23:13:39 +0900

    Use mocking and add more error handling test coverage
    Because the JS import spec defines that imports should cache constants,
    I had to get real weird with the importing of the creatCard function, so
    I could mock it out for error handling tests, but use the real one for
    db transactions.

---
+++ prisma/__mocks__/prisma.ts
@@ -0,0 +1,10 @@
+// taken from https://www.prisma.io/blog/testing-series-1-8eRB5p0Y8o
+import type { PrismaClient } from '@prisma/client'
+import { beforeEach } from 'vitest'
+import { mockDeep, mockReset } from 'vitest-mock-extended'
+
+beforeEach(() => {
+  mockReset(prisma)
+})
+
+export const prisma = mockDeep<PrismaClient>()
--- prisma/queries/__tests__/createCard.spec.ts
+++ prisma/queries/__tests__/createCard.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { prisma } from '../../prisma'
 import createCard from '../createCard'
 import type { Event } from 'electron'
@@ -6,55 +6,54 @@ import type { RPCErrorResponse, RPCSuccessResponse } from 'electron/types'
 import type { Card } from '@prisma/client'
 
 describe('createCardQuery', () => {
-  beforeEach(async () => {
-    // truncate all the data in the db
-    // const tableNames: { name: string }[] = await prisma.$queryRaw<{ name: string }>`
-    //   SELECT name
-    //   FROM sqlite_schema
-    //   WHERE
-    //   type ='table'
-    //   AND name NOT LIKE 'sqlite_%'
-    //   AND name NOT LIKE '_prisma%'
-    //   ;`
-
-    // TODO: find a more automated solution to a db wide clean
-    await prisma.$executeRawUnsafe('DELETE FROM EnglishAnswer;')
-    await prisma.$executeRawUnsafe('DELETE FROM JapaneseAnswer;')
-    await prisma.$executeRawUnsafe('DELETE FROM JapaneseCardSide;')
-    await prisma.$executeRawUnsafe('DELETE FROM EnglishCardSide;')
-    await prisma.$executeRawUnsafe('DELETE FROM Card;')
-  })
-
-  it('creates a card in the db', async () => {
-    await createCard({} as Event, { english: 'cat', kana: 'ねこ', kanji: '猫' })
-
-    const cards = await prisma.card.findMany({
-      include: {
-        englishCardSide: {
-          include: {
-            englishAnswers: true
-          }
-        },
-        japaneseCardSide: {
-          include: {
-            japaneseAnswers: true
-          }
-        }
-      }
+  describe('when the request succeeds', async () => {
+    beforeEach(async () => {
+      // truncate all the data in the db
+      // const tableNames: { name: string }[] = await prisma.$queryRaw<{ name: string }>`
+      //   SELECT name
+      //   FROM sqlite_schema
+      //   WHERE
+      //   type ='table'
+      //   AND name NOT LIKE 'sqlite_%'
+      //   AND name NOT LIKE '_prisma%'
+      //   ;`
+      // TODO: find a more automated solution to a db wide clean
+      await prisma.$executeRawUnsafe('DELETE FROM EnglishAnswer;')
+      await prisma.$executeRawUnsafe('DELETE FROM JapaneseAnswer;')
+      await prisma.$executeRawUnsafe('DELETE FROM JapaneseCardSide;')
+      await prisma.$executeRawUnsafe('DELETE FROM EnglishCardSide;')
+      await prisma.$executeRawUnsafe('DELETE FROM Card;')
     })
 
-    expect(cards.length).toEqual(1)
-    const card = cards[0]
+    it('creates a card in the db', async () => {
+      await createCard({} as Event, { english: 'cat', kana: 'ねこ', kanji: '猫' })
 
-    expect(card.englishCardSide.englishAnswers.length).toEqual(1)
-    expect(card.englishCardSide.englishAnswers[0].answer).toEqual('cat')
+      const cards = await prisma.card.findMany({
+        include: {
+          englishCardSide: {
+            include: {
+              englishAnswers: true
+            }
+          },
+          japaneseCardSide: {
+            include: {
+              japaneseAnswers: true
+            }
+          }
+        }
+      })
 
-    expect(card.japaneseCardSide.japaneseAnswers.length).toEqual(1)
-    expect(card.japaneseCardSide.japaneseAnswers[0].kana).toEqual('ねこ')
-    expect(card.japaneseCardSide.japaneseAnswers[0].kanji).toEqual('猫')
-  })
+      expect(cards.length).toEqual(1)
+      const card = cards[0]
 
-  describe('when the request succeeds', () => {
+      expect(card.englishCardSide.englishAnswers.length).toEqual(1)
+      expect(card.englishCardSide.englishAnswers[0].answer).toEqual('cat')
+
+      expect(card.japaneseCardSide.japaneseAnswers.length).toEqual(1)
+      expect(card.japaneseCardSide.japaneseAnswers[0].kana).toEqual('ねこ')
+      expect(card.japaneseCardSide.japaneseAnswers[0].kanji).toEqual('猫')
+    })
+
     it('returns the card', async () => {
       const response = await createCard({} as Event, {
         english: 'cat',
@@ -68,20 +67,81 @@ describe('createCardQuery', () => {
     })
   })
 
-  describe('when the request fails', () => {
-    it('returns as error', async () => {
-      const response = await createCard({} as Event, {
-        english: undefined,
-        kana: 'ねこ',
-        kanji: '猫'
+  describe('when the request fails', async () => {
+    beforeEach(async () => {
+      vi.doMock('../../prisma.ts')
+      vi.resetModules()
+    })
+
+    describe('with an error object', async () => {
+      it('returns as error', async () => {
+        const { prisma: mockPrisma } = await import('../../__mocks__/prisma')
+        const { default: mockedCreateCard } = await import('../createCard')
+
+        const returnedError = new Error('oh no')
+
+        mockPrisma.card.create.mockImplementation(() => {
+          throw returnedError
+        })
+
+        const response = await mockedCreateCard({} as Event, {
+          english: 'cat',
+          kana: 'ねこ',
+          kanji: '猫'
+        })
+
+        const error = (response as RPCErrorResponse).error
+
+        expect(error).toEqual(returnedError)
       })
+    })
 
-      const error = (response as RPCErrorResponse).error
+    describe('with an object with a message property', () => {
+      it('returns an error with the message', async () => {
+        const { prisma: mockPrisma } = await import('../../__mocks__/prisma')
+        const { default: mockedCreateCard } = await import('../createCard')
 
-      expect(error).toBeInstanceOf(Error)
-      expect(error.message).toContain(
-        'Argument answer for data.englishCardSide.create.englishAnswers.create.answer is missing.'
-      )
+        const returnedError = { message: 'oh no' }
+
+        mockPrisma.card.create.mockImplementation(() => {
+          throw returnedError
+        })
+
+        const response = await mockedCreateCard({} as Event, {
+          english: 'cat',
+          kana: 'ねこ',
+          kanji: '猫'
+        })
+
+        const error = (response as RPCErrorResponse).error
+
+        expect(error).toBeInstanceOf(Error)
+        expect(error.message).toEqual('oh no')
+      })
+    })
+
+    describe('with an unknown error value', () => {
+      it('returns a generic error message', async () => {
+        const { prisma: mockPrisma } = await import('../../prisma')
+        const { default: mockedCreateCard } = await import('../createCard')
+
+        const returnedError = { no: 'idea' }
+
+        mockPrisma.card.create.mockImplementation(() => {
+          throw returnedError
+        })
+
+        const response = await mockedCreateCard({} as Event, {
+          english: 'cat',
+          kana: 'ねこ',
+          kanji: '猫'
+        })
+
+        const error = (response as RPCErrorResponse).error
+
+        expect(error).toBeInstanceOf(Error)
+        expect(error.message).toEqual('something went wrong')
+      })
     })
   })
 })

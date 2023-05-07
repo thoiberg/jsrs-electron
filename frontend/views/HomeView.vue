37850d2f3b8388a0fd100dcef240c3655faccdc8
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-07 15:24:08 +0900

    Remove default create-vue-app code

--- frontend/views/HomeView.vue
+++ frontend/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <p>TODO: This page</p>
   </main>
 </template>

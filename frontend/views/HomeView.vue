82477bfbcb64474e94568e432c8a609281d4baa6
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-08 23:43:07 +0900

    Show number of reviewable cards on the Home Screen

--- frontend/views/HomeView.vue
+++ frontend/views/HomeView.vue
@@ -1,5 +1,31 @@
 <template>
   <main>
-    <p>TODO: This page</p>
+    <p v-if="serverError" class="server-error">{{ serverError }}</p>
+    <p>Reviewable Cards: {{ reviewableCards.length }}</p>
   </main>
 </template>
+
+<script setup lang="ts">
+import type { Card } from '@prisma/client'
+import { onMounted, ref, type Ref } from 'vue'
+
+const reviewableCards: Ref<Card[]> = ref([])
+const serverError = ref('')
+
+onMounted(async () => {
+  const response = await window.electronAPI.getReviewableCards()
+
+  if ('error' in response) {
+    serverError.value = response.error.message
+    return
+  }
+
+  reviewableCards.value = response.data as Card[]
+})
+</script>
+
+<style>
+p {
+  font-size: 2rem;
+}
+</style>

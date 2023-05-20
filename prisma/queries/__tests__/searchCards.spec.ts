291227e9a417354a1b518045d25f2e3f69bd89d6
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-05-20 21:59:58 +0900

    Switch search cards specs to use test db
    I prefer to use a real db over mocking the prisma interactions, as
    mocking prisma doesn't really test anything other than whether I've set
    up the mocks correctly.

--- prisma/queries/__tests__/searchCards.spec.ts
+++ prisma/queries/__tests__/searchCards.spec.ts
@@ -1,74 +1,22 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, beforeEach, vi } from 'vitest'
 
-import searchCards from '../searchCards'
-import { prisma as mockPrisma } from '../../__mocks__/prisma'
-import type { Card } from '@prisma/client'
-
-vi.mock('../../prisma.ts')
+import searchCards, { includeAllCardRelationships } from '../searchCards'
+import { prisma } from '../../prisma'
 
 describe('searchCards', () => {
+  beforeEach(async () => {
+    await prisma.$executeRawUnsafe('DELETE FROM EnglishAnswer;')
+    await prisma.$executeRawUnsafe('DELETE FROM JapaneseAnswer;')
+    await prisma.$executeRawUnsafe('DELETE FROM JapaneseCardSide;')
+    await prisma.$executeRawUnsafe('DELETE FROM EnglishCardSide;')
+    await prisma.$executeRawUnsafe('DELETE FROM Card;')
+  })
+
   describe('when a query is supplied', () => {
     it('searches for the card that matches the query', async () => {
-      const card: Card = {
-        id: '1',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }
-      mockPrisma.card.findMany.mockResolvedValue([card])
+      const card = await createCard('cat', 'ねこ', '猫')
 
       const response = await searchCards({} as Event, { query: 'cat' })
-
-      expect(mockPrisma.card.findMany).toBeCalledWith({
-        where: {
-          OR: [
-            {
-              japaneseCardSide: {
-                japaneseAnswers: {
-                  some: {
-                    kana: {
-                      contains: 'cat',
-                    },
-                  },
-                },
-              },
-            },
-            {
-              japaneseCardSide: {
-                japaneseAnswers: {
-                  some: {
-                    kanji: {
-                      contains: 'cat',
-                    },
-                  },
-                },
-              },
-            },
-            {
-              englishCardSide: {
-                englishAnswers: {
-                  some: {
-                    answer: {
-                      contains: 'cat',
-                    },
-                  },
-                },
-              },
-            },
-          ],
-        },
-        include: {
-          englishCardSide: {
-            include: {
-              englishAnswers: true,
-            },
-          },
-          japaneseCardSide: {
-            include: {
-              japaneseAnswers: true,
-            },
-          },
-        },
-      })
 
       expect(response).toEqual({ data: [card] })
     })
@@ -76,35 +24,59 @@ describe('searchCards', () => {
 
   describe('when a query is not supplied', () => {
     it('returns all cards', async () => {
-      await searchCards({} as Event)
+      const catCard = await createCard('cat', 'ねこ', '猫')
+      const dogCard = await createCard('dog', 'いぬ', '犬')
+      const response = await searchCards({} as Event)
 
-      expect(mockPrisma.card.findMany).toBeCalledWith({
-        include: {
-          englishCardSide: {
-            include: {
-              englishAnswers: true,
-            },
-          },
-          japaneseCardSide: {
-            include: {
-              japaneseAnswers: true,
-            },
-          },
-        },
-      })
+      expect(response).toEqual({ data: [catCard, dogCard] })
     })
   })
 
   describe('when the request fails', () => {
+    beforeEach(() => {
+      vi.doMock('../../prisma.ts')
+      vi.resetModules()
+    })
+
     it('returns the error', async () => {
+      const { prisma: mockPrisma } = await import('../../__mocks__/prisma')
+      const { default: mockedSearchCards } = await import('../searchCards')
+
       const returnedError = new Error('oh no')
       mockPrisma.card.findMany.mockImplementation(() => {
         throw returnedError
       })
 
-      const response = await searchCards({} as Event)
+      const response = await mockedSearchCards({} as Event)
 
       expect(response).toEqual({ error: returnedError })
     })
   })
 })
+
+async function createCard(english: string, kana?: string, kanji?: string) {
+  return await prisma.card.create({
+    data: {
+      japaneseCardSide: {
+        create: {
+          japaneseAnswers: {
+            create: {
+              kana,
+              kanji,
+            },
+          },
+        },
+      },
+      englishCardSide: {
+        create: {
+          englishAnswers: {
+            create: {
+              answer: english,
+            },
+          },
+        },
+      },
+    },
+    include: includeAllCardRelationships,
+  })
+}

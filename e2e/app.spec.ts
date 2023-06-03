368bc0a5c56a15a117d2ae12c7d4fb1ed78e692d
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-03 22:35:27 +0900

    Add delay before running expectations
    Without the delay I was seeing the tests flake more often than not.
    Using setTimeout with a fixed delay is a non-ideal solution, but it
    doesn't look like playwrights native polling works particularly well
    with Electron.

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,25 +1,47 @@
 import { test, expect } from '@playwright/test'
-import { _electron as electron } from 'playwright'
+import { _electron as electron, ElectronApplication } from 'playwright'
 import { findLatestBuild, parseElectronApp } from 'electron-playwright-helpers'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async () => {
+let electronApp: ElectronApplication
+
+test.beforeAll(async () => {
   const latestBuild = findLatestBuild()
   const appInfo = parseElectronApp(latestBuild)
 
-  const electronApp = await electron.launch({
+  electronApp = await electron.launch({
     args: [appInfo.main],
     executablePath: appInfo.executable,
   })
 
-  const window = await electronApp.firstWindow()
+  electronApp.on('window', async (page) => {
+    const filename = page.url()?.split('/').pop()
+    console.log(`Window opened: ${filename}`)
 
-  await window.click('text=Home')
+    // capture errors
+    page.on('pageerror', (error) => {
+      console.error(error)
+    })
 
-  await expect(window.locator('main')).toContainText('Reviewable Cards: 0')
+    // capture console messages
+    page.on('console', (msg) => {
+      console.log(msg.text())
+    })
+  })
+})
 
-  await window.click('text=Add Card')
-
+test.afterAll(async () => {
   await electronApp.close()
+})
+
+test('visits the app root url', async () => {
+  const page = await electronApp.firstWindow()
+
+  // TODO: find a better way to reliably wait until the page is loaded
+  setTimeout(async () => {
+    await page.click('text=Home')
+
+    await expect(page.locator('main')).toContainText('Reviewable Cards: 0')
+
+    await page.click('text=Add Card')
+  }, 1000)
 })
